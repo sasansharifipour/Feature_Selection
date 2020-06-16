eb7e19be0e4f40e2532cb8759af71ca4877c5e5a
Sasan Sharifipour <sasansharifipour@gmail.com>
2020-06-16 11:31:57 +0430

    add model operation class

--- CommonCode.py
+++ CommonCode.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pickle
 
 class covariance_operations:
 
@@ -9,7 +10,7 @@ class covariance_operations:
     
     def calculate_covariance():
 
-        if (self.data.shape[0] == feature_cnt)
+        if (self.data.shape[0] == feature_cnt):
             self.calculated_covariance = np.cov(self.data)
 
         elif (self.data.shape[1] == feature_cnt):
@@ -38,4 +39,36 @@ class covariance_operations:
         
         
         
+class autoencoder_operations:
 
+    def __init__(self, data, feature_cnt, hidden_layer_size, file_path):
+        self.data = data
+        self.feature_cnt = feature_cnt
+        self.hidden_layer_size
+        self.file_path = file_path
+
+    def create_model():
+        encoding_dim = self.hidden_layer_size
+        input_img = Input(shape=(self.feature_cnt,))
+        encoded = Dense(encoding_dim, activation='relu')(input_img)
+        decoded = Dense(self.feature_cnt, activation='sigmoid')(encoded)
+        autoencoder = Model(input_img, decoded)
+        autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+        self.model = autoencoder
+        return self.model
+
+    def train_model():
+        self.model.fit(self.data, self.data,
+                epochs=100,
+                batch_size=256,
+                shuffle=True)
+
+    def save_model():
+        self.model.save(self.file_path)
+
+    def load_model():
+        self.model = load_model(file_path)
+
+    def get_model():
+        return self.model
